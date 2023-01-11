eaaaa35703bd7aebc09fcd1eebb04fe9cee46bb6
Sonu Daryani <100096513+sonu7524@users.noreply.github.com>
2023-01-11 18:30:58 +0530

    main file exceute successfully

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,26 @@
 package com.driver;
 
 public class Main {
+  public static void main(String[] args) {
+        Product p = new Product();
+        //int method with two actual parameters
+        p.product(768,2500);
+        //int method with three parameters
+        p.product(768,2500, 1800);
+        //double method 
+        p.product(7.98,2.25);
 
+    }
+
+    static class Product{
+        public int product(int x, int y){
+            return x-y;
+        }
+        public int product(int x, int y, int z){
+            return x+y+z;
+        }
+        public double product(double x, double y){
+            return x*y;
+        }
+    }
 }
