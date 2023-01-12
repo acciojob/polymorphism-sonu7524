4feba22f7534c89facb015c193486726c43d09eb
sonu7524 <sonudaryani135@gmail.com>
2023-01-12 22:06:22 +0530

    sol-2

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -15,17 +15,14 @@ public class Main {
   public static class Product{
     //method 1 with two formal parameters
     public int product(int x, int y){
-      System.out.println("Method 1");
       return 0;
     }
     //method 2 with same name but having three formal parameters
     public int product(int x, int y, int z){
-      System.out.println("Method 2");
       return 0;
     }
     //method 3 with same name but having different return type
     public double product(double x, double y){
-      System.out.println("Method 3");
       return 0;
     }
   }
